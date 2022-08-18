660f4b5fbe799bfe69ca9bb56c40baacc662d963
Oleg Blednov <o.a.blednov@foxford.ru>
2022-08-19 02:11:54 +0300

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import {PullRequestChangedFile, Repository} from "@octokit/graphql-schema";
+import {GitHub} from "@actions/github/lib/utils";
 
 
 async function main() {
     const owner = core.getInput("owner");
     const repository = core.getInput("repository");
     const token = core.getInput("token");
-    const pull_number = core.getInput("pull_number")
+    const pullNumber = parseInt(core.getInput("pull_number"))
     const octokit = github.getOctokit(token);
+    const params = [octokit, owner, repository, pullNumber]
+    const changedFiles = await getPullRequestChangedFiles(...params)
+    for (const changedFile of changedFiles) {
+        await getChangedLineParents(...params, changedFile.path);
+    }
+}
+
+async function getPullRequestChangedFiles(octokit: InstanceType<typeof GitHub>, owner: string, name: string, pullNumber: number): Promise<PullRequestChangedFile[]> {
     const query = `
         {
-            repository(owner: "${owner}", name: "${repository}") {
-                pullRequest(number:${pull_number}) {
+            repository(owner: "${owner}", name: "${name}") {
+                pullRequest(number:${pullNumber}) {
                     files(first:100) {
                         nodes { path }
                         pageInfo { endCursor }
@@ -19,9 +29,56 @@ async function main() {
                 }
             }
         }
-    `
-    const result = await octokit.graphql(query);
-    console.log(result);
+    `;
+    const repository = await octokit.graphql(query) as Repository;
+    return repository.pullRequest.files.nodes;
+}
+
+async function getChangedLineParents(octokit: InstanceType<typeof GitHub>, owner: string, name: string, pullNumber: number, changedFilePath: string) {
+    const query = `
+        {    
+            repository(owner: "${owner}", name: "${name}") {
+                pullRequest(number:${pullNumber}) {
+                    potentialMergeCommit {
+                    # mergeCommit {
+                        id
+                        history(first:2, path:"${changedFilePath}") {
+                            nodes {
+                                blame(path:"${changedFilePath}") {
+                                    ranges {
+                                        startingLine
+                                        endingLine
+                                        commit {
+                                            id
+                                            message
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `;
+    const repository = await octokit.graphql(query) as Repository;
+    const mergeCommit = repository.pullRequest.mergeCommit || repository.pullRequest.potentialMergeCommit;
+    const changedLines: number[] = [];
+    mergeCommit.history.nodes.forEach(commit => {
+        commit.blame.ranges.forEach(blame => {
+            const startingLine = blame.startingLine;
+            const endingLine = blame.endingLine;
+            if (blame.commit.id == mergeCommit.id) {
+                changedLines.push(startingLine, endingLine);
+            } else {
+                for (const lineNumber of changedLines) {
+                    if (lineNumber <= startingLine && lineNumber <= endingLine) {
+                        console.log(`Caught: ${blame.commit.messageHeadline}`)
+                    }
+                }
+            }
+        })
+    })
 }
 
 main().catch(error => {
